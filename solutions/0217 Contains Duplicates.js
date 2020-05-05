cfe001b2f53cb4eee197c775edb7fac3bf3fab6f
Alexander Kulik <spirit.ixi@gmail.com>
2020-05-06 00:23:38 +0200

    Remove logs

--- solutions/0217 Contains Duplicates.js
+++ solutions/0217 Contains Duplicates.js
@@ -32,8 +32,3 @@ var containsDuplicate = function(nums) {
 
     return false;
 };
-
-
-console.log(containsDuplicate([1,2,3,1]));
-console.log(containsDuplicate([1,2,3,4]));
-console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]));
