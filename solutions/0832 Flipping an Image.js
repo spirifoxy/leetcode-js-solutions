b3c55c9c4cdf8112fbd65e6bbe353da74938feb0
Alexander Kulik <spirit.ixi@gmail.com>
2020-05-12 01:14:43 +0200

    Add solution #832

---
+++ solutions/0832 Flipping an Image.js
@@ -0,0 +1,46 @@
+/*
+https://leetcode.com/problems/flipping-an-image/
+
+Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+To flip an image horizontally means that each row of the image is reversed. 
+For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+Example 1:
+    Input: [[1,1,0],[1,0,1],[0,0,0]]
+    Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+Example 2:
+    Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+    Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+
+Notes:
+    1 <= A.length = A[0].length <= 20
+    0 <= A[i][j] <= 1
+*/
+
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(A) {
+    for (let i = 0; i < A.length; i++) {
+        const arr = A[i];
+        let start = 0;
+        let end = arr.length - 1;
+        while (start < Math.floor((arr.length + 1) / 2)) {
+            const tmp = arr[start] ^ 1;
+            arr[start] = arr[end] ^ 1;
+            arr[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+    return A;
+};
